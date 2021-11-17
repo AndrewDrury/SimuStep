1b40b09a0f78ba7697526b80b3215cb502aa4e16
Andrew Drury <drurya99@gmail.com>
2021-11-17 18:02:14 -0500

    Restructure repo, init finished

---
+++ io.py
@@ -0,0 +1,12 @@
+# DEFINE INPUTS
+pot     # Potentiometer
+emg     # EMG Sensor
+imu     # IMU Sensor
+btnMode # Mode-changing button
+
+# DEFINE OUTPUTS
+ledBlue     # Blue LED - indicating calibration mode
+ledGreen    # Green LED - indicating walking mode
+ledYellow   # Yellow LED - indicating rehabilitation mode
+ledPower    # Power LED - indicating the power state of the device
+electrodes  # 2 Electrodes (top and bottom)
